8b695da345f1053c7a9272a84afa32de8bf42f66
Shawn Canavan <shawncanavan@Shawns-MacBook-Pro-2.local>
2023-03-07 15:11:22 -0500

    almost finished header basics

--- src/components/Header/Navbar/Navbar.jsx
+++ src/components/Header/Navbar/Navbar.jsx
@@ -1,21 +1,21 @@
 import React from "react";
-import bp from '../../../assets/img/bp.png'
+import bp from '../../../assets/img/bpw.png'
+import './navbar.css'
 
 //bg-[#00ADBB]
 
 const classes = {
-    listItem: "block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-white dark:bg-blue-600 md:dark:bg-transparent", 
-    unorderedList: "flex flex-col mt-4 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-transparent dark:bg-gray-800 md:dark:bg-transparent dark:border-gray-700"
+    listItem: "block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-white-700 md:p-0 md:dark:text-white dark:bg-blue-600 md:dark:bg-transparent hover:text-xl", 
+    unorderedList: "flex flex-col mt-4 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:text-lg md:font-medium md:border-0 md:bg-transparent dark:bg-gray-800 md:dark:bg-transparent dark:border-gray-700"
 }
 
 function Navbar(){
     return(
   
-
-<nav class="p-3 border-gray-200 rounded bg-gray-50 dark:bg-gray-800 dark:border-gray-700">
+<nav class="p-3 rounded bg-[#00ADBB]">
   <div class="container flex flex-wrap items-center justify-between mx-auto">
     <a href="#" class="flex items-center">
-        <img src={bp} class="w-5/6" alt="Bramson Logo" />
+        <img src={bp} class="w-52" alt="Bramson Logo" />
         <span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white"></span>
     </a>
     <button data-collapse-toggle="navbar-solid-bg" type="button" class="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-solid-bg" aria-expanded="false">
