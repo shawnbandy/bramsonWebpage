5661d3a16a5f419e58fb857e23c3f0dcae40d1d1
Shawn Canavan <shawncanavan@Shawns-MacBook-Pro-2.local>
2023-03-08 07:54:28 -0500

    working on section 1

--- src/components/Content/Sections/Section_1/Section1.jsx
+++ src/components/Content/Sections/Section_1/Section1.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import './section1.css'
+import Typewriter from 'typewriter-effect';
 
 /*
 *This section will be for the video/gif that is currently on the Website
@@ -12,57 +13,29 @@ let speed = 10000;
 
 function Section1({sectionClass}){
 
-    const [index, setIndex] = useState(0)
-    const [offset, setOffset] = useState(0)
-    const [forwards, setForwards] = useState(true);
-    const [skipCount, setSkipcount] = useState(0)
-    const [skipDelay, setSkipDelay] = useState(15)
-    const [part, setPart] = useState('')
-    const [word, setWord] = useState('test')
-
-    function animate() { 
-    console.log('testing')
-    setInterval(() => {
-        if (forwards){
-            if(offset >= words[index].length){
-                setSkipcount(skipCount + 1)
-                if (skipCount == skipDelay){
-                    setForwards(false)
-                    setSkipcount(0)
-                }
-            }
-        } else {
-            if (offset == 0){
-                setForwards(true)
-                setIndex(index + 1);
-                setOffset(0)
-                if(index >= words.length){
-                    setIndex(0);
-                }
-            }
-        }
-        setPart(words[index].substring(0, offset))  
-        if (skipCount == 0) {
-          if (forwards) {
-            setOffset(offset + 1);
-          }
-          else {
-            setOffset(offset - 1);
-          }
-        }
-        setWord(part);
-    }, speed) 
-}
-
-    useEffect(() => {
-        animate()
-    })
-
     return(
         <section className={sectionClass}>
-            <div className="flex">
-                <span className="text-9xl">WE</span>
-                <div className="wordDiv">{word}</div>
+            <div className="text-2xl text-white">
+                <h1>At Bramson</h1>
+            </div>
+            <div className="grid grid-cols-2">
+                <div className="grid justify-items-end mr-8">
+                    <span className="text-9xl text-white">WE</span>
+                </div>
+                <div className="text-9xl text-[#00ADBB]">
+                    <Typewriter
+                        options={{
+                        strings: ['Create', 'Produce', 'Deliver'],
+                        autoStart: true,
+                        loop: true,
+                    }}/>
+                </div>
+            </div>
+            <div className="text-2xl text-white">
+                <p>Not all meetings are the same. We are here to help tell your story, reach your audience and achieve your goals. From events in hotel ballrooms to meetings in family living rooms... we are Bramson Productions.</p>
+                <br></br>
+                <br></br>
+                <p>Nice to meet you! To learn more, say hello.  </p>
             </div>
         </section>
     )
